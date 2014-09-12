65c19e58543a2f492fe1dcb16638bb942c481e32
Tim Cuthbertson <tim@gfxmonk.net>
2014-09-12 21:53:34 +1000

    styling & arbitrary selection

--- gsel.ml
+++ gsel.ml
@@ -7,19 +7,41 @@ let debug fmt =
     then (prerr_string "[gsel]: "; Printf.kfprintf (fun ch -> output_char ch '\n'; flush ch) stderr fmt)
     else Printf.ifprintf stderr fmt
 
+type entry = {
+  text : string;
+  match_text : string;
+}
+
 module SortedSet = struct
-  type t = string list
-  let score = String.length
+  type t = entry list
+  let cmp a b =
+    let a = a.text and b = b.text in
+    let len_diff = String.length a - String.length b in
+    if len_diff <> 0
+      then len_diff
+      else String.compare a b
+
   let create () : t = []
-  let rec add (existing : t) (elem : string) =
+  let rec add (existing : t) (elem : entry) =
     match existing with
       | [] -> [elem]
       | head :: tail ->
           if elem = head then existing (* ignore duplicates *) else
-            if (score elem) < (score head)
+            if (cmp elem head) <= 0
               then elem :: existing
               else head :: (add tail elem)
 end
+
+let rgb a b c = `RGB (a lsl 8, b lsl 8, c lsl 8)
+let grey l = let l = l lsl 8 in `RGB (l,l,l)
+let font_scale = 1204
+
+let all_states col = [
+  `INSENSITIVE, col;
+  `NORMAL, col;
+  `PRELIGHT, col;
+  `SELECTED, col;
+]
 
 let main () =
   ignore (GMain.init ());
@@ -28,30 +50,66 @@ let main () =
   let gui_end, wakener = Lwt.wait () in
   (* let quit = Lwt.wakeup wakener in *)
 
+  let colormap = Gdk.Color.get_system_colormap () in
+  let text_color = Gdk.Color.alloc ~colormap (grey 190) in
+
   let window = GWindow.window
     ~border_width: 10
     ~screen:(Gdk.Screen.default ())
     ~width: 600
-    ~height: 400
+    ~height: 500 (* XXX set window height based on size of entries *)
     ~decorated: false
     ~position: `CENTER
     ~title:"gsel"
     () in
 
-  let quit () : unit =
+  let quit status : unit =
     (* window#destroy; *)
     Lwt.wakeup wakener ();
-    exit 0
+    exit status
     (* () *)
   in
+
   let vbox = GPack.vbox ~spacing: 10 ~packing:window#add () in
-  let input = GEdit.entry ~activates_default: true ~packing:vbox#pack () in
-  let glist = GList.liste ~packing:(vbox#pack ~expand:true) () in
-  glist#set_selection_mode `NONE;
+
+  (* using two extra boxes seems a rather hacky way of getting some padding... *)
+  let input_box = GBin.event_box ~packing:vbox#pack ~border_width:0 () in
+  let input_box_inner = GBin.event_box ~packing:input_box#add ~border_width:6 () in
+  let input = GEdit.entry ~activates_default:true ~packing:input_box_inner#add () in
+  input#set_has_frame false;
+  let columns = new GTree.column_list in
+  let column = columns#add Gobject.Data.string in
+  let list_store = GTree.list_store columns in
+  let tree_view = GTree.view
+    ~model:list_store
+    ~border_width:0
+    ~show:true
+    ~enable_search:false
+    ~packing:(vbox#pack ~expand:true) () in
+  (* tree_view#set_selection_mode `NONE; *)
+  let column_view = GTree.view_column
+    ~renderer:((GTree.cell_renderer_text [
+      `FOREGROUND_GDK text_color;
+      `SIZE_POINTS 12.0
+    ]), [("text", column)]) () in
+  ignore (tree_view#append_column (column_view));
+  tree_view#set_headers_visible false;
+  let tree_selection = tree_view#selection in
 
   let all_items = ref (SortedSet.create ()) in
   let shown_items = ref [] in
   let last_query = ref "" in
+  let selected_index = ref 0 in
+
+  ignore (tree_selection#connect#changed ~callback:(fun () ->
+    List.iter (fun path ->
+      match GTree.Path.get_indices path with
+        | [|i|] ->
+          debug "selected index is now %d" i;
+          selected_index := i
+        | _ -> assert false
+    ) tree_selection#get_selected_rows
+  ));
 
   let matches needle haystack =
     (* debug ("Looking for "^ needle ^" in "^haystack); *)
@@ -77,23 +135,28 @@ let main () =
         loop (String.get needle 0, 0) (String.get haystack 0, 0)
   in
 
-
   let redraw () =
-    glist#clear_items ~start:0 ~stop:(List.length glist#children);
+    list_store#clear ();
     shown_items := List.filter (fun item ->
-      matches !last_query item
+      matches !last_query item.match_text
     ) !all_items;
     debug "redraw! %d items of %d" (List.length !shown_items) (List.length !all_items);
     flush stdout;
-    List.iteri (fun i label ->
-      if i < 20 then
-        let (_:GList.list_item) = GList.list_item ~label ~packing:glist#append () in
-        ()
-    ) !shown_items
+    (* TODO: overwrite text, rather than always recreating each item? *)
+    let rec loop i entries =
+      match entries with
+        | [] -> ()
+        | {text} :: tail ->
+          let added = list_store#append () in
+          list_store#set ~row:added ~column text;
+          let i = i - 1 in
+          if i > 0 then loop i tail else ()
+    in
+    loop 20 !shown_items
   in
 
   let update_query = fun text ->
-    last_query := text;
+    last_query := String.lowercase text;
     redraw ()
   in
 
@@ -102,7 +165,7 @@ let main () =
     let read_loop =
       let lines = Lwt_io.read_lines Lwt_io.stdin in
       Lwt_stream.iter (fun line ->
-        all_items := SortedSet.add !all_items line;
+        all_items := SortedSet.add !all_items {text=line; match_text=String.lowercase line};
       ) lines
     in
     let update_loop =
@@ -114,31 +177,59 @@ let main () =
 
   ignore (input#connect#notify_text update_query);
 
+  let selection_made () =
+    let selected = try Some (List.nth !shown_items !selected_index) with Failure _ -> None in
+    begin match selected with
+      | Some entry -> print_endline entry.text; quit 0
+      | None -> ()
+    end
+  in
+
+  (* XXX are there constants, or do we really have to parse thse at runtime? *)
   let (esc, _) = GtkData.AccelGroup.parse "Escape" in
   let (ret, _) = GtkData.AccelGroup.parse "Return" in
   ignore (window#event#connect#key_release (fun evt ->
     let key = GdkEvent.Key.keyval evt in
-    (* XXX constant / enum somewhere? *)
     debug "Key: %d" key;
     let () = match key with
-      |k when k=esc -> quit ()
-      |k when k=ret ->
-        let selected = try Some (List.hd !shown_items) with Failure _ -> None in
-        begin match selected with
-          | Some str -> print_endline str; quit ()
-          | None -> ()
-        end
+      |k when k=esc -> quit 1
+      |k when k=ret -> selection_made ()
       | _ -> ()
     in
     true
   ));
-
-  ignore (window#event#connect#delete ~callback:(fun _ -> quit (); true));
+  ignore (tree_view#connect#row_activated (fun _ _ -> selection_made ()));
+  ignore (window#event#connect#delete (fun _ -> quit 1; true));
+  (* XXX set always-on-top *)
   window#set_skip_taskbar_hint true;
   (* window#set_skip_pager_hint true; *)
-  ignore (window#connect#destroy ~callback:quit);
   (* button#connect#clicked ~callback:(fun () -> prerr_endline "Hello World"); *)
   (* button#connect#clicked ~callback:window#destroy; *)
+
+  (* stylings! *)
+  let input_bg = grey 40 in
+
+  let ops = new GObj.misc_ops window#as_widget in
+  ops#modify_bg [`NORMAL, grey 10];
+
+  let ops = new GObj.misc_ops vbox#as_widget in
+  ops#modify_bg [`NORMAL, grey 100];
+
+  let ops = new GObj.misc_ops tree_view#as_widget in
+  ops#modify_base [`NORMAL, grey 25];
+  ops#modify_base [`SELECTED, rgb 37 89 134];
+
+  let ops = new GObj.misc_ops input_box#as_widget in
+  ops#modify_bg [`NORMAL, input_bg];
+
+  let ops = new GObj.misc_ops input#as_widget in
+  let font = ops#pango_context#font_description in
+  Pango.Font.modify font ~weight:`BOLD ~size:(10 * font_scale) ();
+  ops#modify_font font;
+  ops#modify_base [`NORMAL, input_bg];
+  ops#modify_text [`NORMAL, grey 250];
+
+
   window#show ();
   (* Main.main () *)
 
